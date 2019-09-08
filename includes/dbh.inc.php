5ef37e6b35a18bffbf767cc98cb9f252c70098f2
Filip <filip.cesnek@gmail.com>
2019-09-08 18:53:08 +0200

    Update dbh.inc.php

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,11 +1,16 @@
 <?php
+  // $dbServername = "localhost";
+  // $dbUsername = "root";
+  // $dbPassword = "";
+  // $dbName = "webprog";
 
-  $dbServername = "localhost";
-  $dbUsername = "root";
-  $dbPassword = "";
-  $dbName = "webprog";
+  $dbServername = getenv('DB_SERVER_NAME');
+  $dbUsername = getenv('DB_USERNAME');
+  $dbPassword = getenv('DB_PASSWORD');
+  $dbName = getenv('DB_NAME');
+  $dbPort = getenv('DB_PORT');
 
-  $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword);
+  $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName, $dbPort);
 
   if(!$conn) {
     die("Connection failed: ".mysqli_connect_error());
